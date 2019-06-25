1d848a83c985265aef83f23bd43b15277f18c426
anvien <anrial@126.com>
2019-06-25 11:29:11 +0800

    Add files via upload

--- engine/bing.py
+++ engine/bing.py
@@ -1,21 +1,24 @@
-from util import Util
-from engine import Engine
+from util.util import Util
+from engine.engine import Engine
 import time
 
 
 class Bing(Engine):
     def __init__(self, web_driver, start_url, storing_folder):
-        Engine.__init__(self, web_driver, start_url, storing_folder)
+        Engine.__init__(self, web_driver)
+        self.start_url = start_url
+        self.storing_folder = storing_folder
 
     def retrieve_image(self):
         Engine.retrieve_image(self)
+        self.web_driver.get(self.start_url)
 
         url = self.web_driver.find_element_by_class_name("mainImage accessible nofocus")
         current_index = 0
         while url is not None:
             url = url.get_attribute('src')
             image_format = url.split('.')[-1]
-            print'IMAGE URL: ', url
+            print('IMAGE URL: ', url)
             image_name = str(current_index) + '.' + image_format
             if Util.download_image(url, self.storing_folder, image_name):
                 current_index += 1
