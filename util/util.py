12e87218b8c62a771903c06652b7bf285e49e58f
anvien <anrial@126.com>
2019-06-25 11:22:10 +0800

    Add files via upload

---
+++ util/util.py
@@ -0,0 +1,37 @@
+import urllib.request
+from util.user_agents import UserAgent
+import os
+
+
+class Util:
+    @staticmethod
+    def create_dir(dir):
+        is_success_creation = False
+        try:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+                is_success_creation = True
+        except Exception as e:
+            print(e)
+        return is_success_creation
+
+    @staticmethod
+    def download_image(url, path, name):
+        is_success_downloading = False
+        # try:
+        headers = {'User-Agent': UserAgent.get_agent_randomly()}
+        request = urllib.request.Request(url=url, headers=headers)
+        binary_data = urllib.request.urlopen(request).read()
+        if len(binary_data) > 1000:
+            if not os.path.exists(path):
+                Util.create_dir(path)
+            with open(os.path.join(path, name), 'wb') as f:
+                f.write(binary_data)
+                f.flush()
+                f.close()
+                is_success_downloading = True
+        else:
+            print('Image too small, skipped.')
+        # except Exception as e:
+        #     print(e)
+        return is_success_downloading
